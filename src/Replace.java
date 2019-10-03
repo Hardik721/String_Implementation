1ade10a68953065bcc943b1fc9af013340528533
Hardik721 <hardik.gupta_cs18@gla.ac.in>
2019-10-03 23:42:42 +0530

    init commit

--- src/Replace.java
+++ src/Replace.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 
-public class Main {
+public class Replace {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         String str = obj.nextLine();
+        String str1 = str.replaceFirst("[aeiou]", "*");
+        System.out.println(str1);
 
     }
+}
