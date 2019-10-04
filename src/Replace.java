5cf0145c20fd8df15c96bda6978827bea5fb5717
Hardik721 <hardik.gupta_cs18@gla.ac.in>
2019-10-04 22:18:03 +0530

    replaceall function replaces the characters which are passed in it

--- src/Replace.java
+++ src/Replace.java
@@ -4,6 +4,9 @@ public class Replace {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         String str = obj.nextLine();
+        String str1 = str.replaceAll("[aeiouAEIOU]", "*");
+        System.out.println(str1);
+
 
     }
 }
