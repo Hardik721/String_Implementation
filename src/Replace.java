bff818572cd629ad7f577ca54547065ccb078688
Hardik721 <hardik.gupta_cs18@gla.ac.in>
2019-10-04 21:41:18 +0530

    it will take input as a string

--- src/Replace.java
+++ src/Replace.java
@@ -4,7 +4,7 @@ public class Replace {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         String str = obj.nextLine();
-        String str1 = str.replaceFirst("[aeiou]", "*");
+        String str1 = str.replaceAll("[aeiouAEIOU]", "*");
         System.out.println(str1);
 
     }
