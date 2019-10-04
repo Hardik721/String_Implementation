8f0c9713fce42512f237c10cf97c1c0f46e83e1e
Hardik721 <hardik.gupta_cs18@gla.ac.in>
2019-10-04 21:59:06 +0530

    typecasting of 'character' variable is done to print ASCII value of characters present in the string

--- src/Totalweight.java
+++ src/Totalweight.java
@@ -7,6 +7,7 @@ public class Totalweight {
         int sum = 0;
         for (int i = 0; i <= str.length(); i++) {
             char character = str.charAt(i);
+            sum = sum + (int) character;
         }
 
 
