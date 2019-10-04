266e2fb987e14ae5b8faf531fce9a858af347261
Hardik721 <hardik.gupta_cs18@gla.ac.in>
2019-10-04 21:55:20 +0530

    for loop is initiated and it will run upto length of string

--- src/Totalweight.java
+++ src/Totalweight.java
@@ -4,6 +4,9 @@ public class Totalweight {
     public static void main(String[] args) {
         Scanner object = new Scanner(System.in);
         String str = object.nextLine();
+        int sum = 0;
+        for (int i = 0; i <= str.length(); i++)
+
 
     }
 }
