7f4cacca9d51554a962bfb0345ba74d84efd0d1d
Hardik721 <hardik.gupta_cs18@gla.ac.in>
2019-10-04 21:57:20 +0530

    it extracts the character from the string one by one till the string ends

--- src/Totalweight.java
+++ src/Totalweight.java
@@ -5,7 +5,9 @@ public class Totalweight {
         Scanner object = new Scanner(System.in);
         String str = object.nextLine();
         int sum = 0;
-        for (int i = 0; i <= str.length(); i++)
+        for (int i = 0; i <= str.length(); i++) {
+            char character = str.charAt(i);
+        }
 
 
     }
