05d16a37ade722d114ce7c688c03c07b26328e5d
Hardik721 <hardik.gupta_cs18@gla.ac.in>
2019-10-04 23:25:44 +0530

    it will take input as a string

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner object = new Scanner(System.in);
         String str = object.nextLine();
-        System.out.println("index:\t");
+        System.out.print("index:\t");
         for (int i = 0; i < str.length(); i++) {
-
+            System.out.print(i + "\t");
+        }
+        System.out.println("");
+        System.out.print("chars:\t");
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i) + "\t");
         }
     }
 }
---
+++ src/Substring.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Substring {
+    public static void main(String[] args) {
+        Scanner object = new Scanner(System.in);
+        String str = object.nextLine();
+
+    }
+}
