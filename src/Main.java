6aa03504772535f0998e9fa668152e4689650ef8
Hardik721 <hardik.gupta_cs18@gla.ac.in>
2019-10-04 23:00:33 +0530

    for loop is initiated and run upto length of string

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner object = new Scanner(System.in);
         String str = object.nextLine();
+        System.out.println("index:\t");
+        for (int i = 0; i < str.length(); i++) {
+
+        }
     }
 }
