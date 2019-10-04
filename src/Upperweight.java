4cdf4069cbbb7e458a51bd347ba059fa57487cef
Hardik721 <hardik.gupta_cs18@gla.ac.in>
2019-10-04 22:08:31 +0530

    if condition is applied to check the uppercase letters present in the string

--- src/Upperweight.java
+++ src/Upperweight.java
@@ -7,7 +7,10 @@ public class Upperweight {
         int sum = 0;
         for (int i = 0; i < str.length(); i++) {
             char character = str.charAt(i);
-
+            if (character >= 'A' && character <= 'Z') {
+                sum = sum + (int) character;
+            }
         }
+        System.out.println(sum);
     }
 }
