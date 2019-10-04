9a3d3e916108b5c05d1d835313c867bf5713cb39
Hardik721 <hardik.gupta_cs18@gla.ac.in>
2019-10-04 22:01:49 +0530

    take input as a string

--- src/Totalweight.java
+++ src/Totalweight.java
@@ -5,10 +5,11 @@ public class Totalweight {
         Scanner object = new Scanner(System.in);
         String str = object.nextLine();
         int sum = 0;
-        for (int i = 0; i <= str.length(); i++) {
+        for (int i = 0; i < str.length(); i++) {
             char character = str.charAt(i);
             sum = sum + (int) character;
         }
+        System.out.println(sum);
 
 
     }
--- src/Upperweight.java
+++ src/Upperweight.java
@@ -1,2 +1,8 @@
+import java.util.Scanner;
+
 public class Upperweight {
+    public static void main(String[] args) {
+        Scanner object = new Scanner(System.in);
+        String str = object.nextLine();
+    }
 }
