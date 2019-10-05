e594fc5bcc6c61c347fc6d6fe544e8eb86f1a592
Hardik721 <hardik.gupta_cs18@gla.ac.in>
2019-10-05 18:37:15 +0530

    for loop initiated ,runs upto the lenght of the string charat() is to extarct the character one by one from the string

--- src/Absoluteweight.java
+++ src/Absoluteweight.java
@@ -4,5 +4,11 @@ public class Absoluteweight {
     public static void main(String[] args) {
         Scanner object = new Scanner(System.in);
         String str = object.nextLine();
+        int sumofupperweight = 0;
+        int sumoflowerweight = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char character = str.charAt(i);
+
+        }
     }
 }
