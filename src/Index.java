0b1072c57cbd89678809395820bb0d192d862bad
Hardik721 <hardik.gupta_cs18@gla.ac.in>
2019-10-05 19:37:42 +0530

    it takes input of character of which index is to be found

--- src/Index.java
+++ src/Index.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Index {
     public static void main(String[] args) {
         Scanner object = new Scanner();
+        String str = object.nextLine();
+        char character = object.next().charAt(0);
 
     }
 }
