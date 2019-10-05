85f93dee645a841f52da56e0c9bbee447f3e1802
Hardik721 <hardik.gupta_cs18@gla.ac.in>
2019-10-05 18:41:48 +0530

    if condition to check whether the character is in uppercase or not and to calculate the upperweight

--- src/Absoluteweight.java
+++ src/Absoluteweight.java
@@ -8,6 +8,9 @@ public class Absoluteweight {
         int sumoflowerweight = 0;
         for (int i = 0; i < str.length(); i++) {
             char character = str.charAt(i);
+            if (character >= 'A' && character <= 'Z') {
+                sumofupperweight = sumofupperweight + (int) character;
+            }
 
         }
     }
