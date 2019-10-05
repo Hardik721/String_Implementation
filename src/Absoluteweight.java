4df6b4577842607e5e20ab71fc04baa7933703d5
Hardik721 <hardik.gupta_cs18@gla.ac.in>
2019-10-05 18:32:33 +0530

    input is taken as a String

---
+++ src/Absoluteweight.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Absoluteweight {
+    public static void main(String[] args) {
+        Scanner object = new Scanner(System.in);
+        String str = object.nextLine();
+    }
+}
